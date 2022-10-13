e4cb0f11569cde53632ff6c0874ccff6dc9659dc
Pedro Henrique da Silva <pedro_rike@hotmail.com.br>
2022-10-13 20:13:58 -0300

    feat: create form page

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,21 +1,29 @@
 import { clsx } from "clsx";
 import { Slot } from "@radix-ui/react-slot";
-import { ReactNode } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode;
   asChild?: boolean;
+  className?: string;
 }
 
-export const Button = ({ children, asChild }: ButtonProps) => {
+export const Button = ({
+  children,
+  asChild,
+  className,
+  ...props
+}: ButtonProps) => {
   const Comp = asChild ? Slot : "button";
 
   return (
     <Comp
       className={clsx(
-        `py-4 px-3 bg-cyan-500 rounded font-semibold text-black 
-         transition-colors text-sm w-full hover:bg-cyan-300 focus:ring-2 ring-white`
+        `py-3 px-4 bg-cyan-500 rounded font-semibold text-black 
+         transition-colors text-sm w-full hover:bg-cyan-300 focus:ring-2 ring-white`,
+        className
       )}
+      {...props}
     >
       {children}
     </Comp>
